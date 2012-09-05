6668cce96362eb42252ae8a5463a5ec489c8cb33
Lucas <lucass@gmail.com>
2012-09-04 22:05:50 -0300

    Adding functional test

--- src/test/java/com/github/junrar/testUtil/JUnRarTestUtil.java
+++ src/test/java/com/github/junrar/testUtil/JUnRarTestUtil.java
@@ -18,21 +18,32 @@
  */
 package com.github.junrar.testUtil;
 
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.fail;
+
 import java.io.ByteArrayOutputStream;
 import java.io.File;
 import java.io.IOException;
+import java.io.InputStream;
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
 
+import org.apache.commons.io.FileUtils;
+import org.apache.commons.io.IOUtils;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
+import org.junit.AfterClass;
+import org.junit.BeforeClass;
+import org.junit.Test;
 
 import com.github.junrar.Archive;
 import com.github.junrar.exception.RarException;
 import com.github.junrar.exception.RarException.RarExceptionType;
 import com.github.junrar.io.ReadOnlyAccessFile;
 import com.github.junrar.rarfile.FileHeader;
+import com.github.junrar.testutil.ExtractArchive;
 
 /**
  * DOCUMENT ME
@@ -52,6 +63,9 @@ public class JUnRarTestUtil
 	
 	private static List<String> unsupportedFiles = new ArrayList<String>();
 	
+	private static  File tempFolder;
+	private static boolean errorsOcurred;
+	
 	public static void main(String[] args)
 	{
 		if(args.length!=1){
@@ -69,6 +83,38 @@ public class JUnRarTestUtil
 		}
 		printSummary();
 
+	}
+	
+	@BeforeClass
+	public static void setupFunctionalTests() throws IOException{
+		tempFolder = File.createTempFile("FOOOOOOO", "BAAAARRRR");
+		tempFolder.delete();
+		tempFolder.mkdir();
+	}
+	
+	@Test
+	public void unrarFile_FileContentsShouldMatchExpected() throws IOException{
+		InputStream resourceAsStream = JUnRarTestUtil.class.getResourceAsStream("test.rar");
+		File testRar = new File(tempFolder,"test.rar");
+        FileUtils.writeByteArrayToFile(testRar, IOUtils.toByteArray(resourceAsStream));
+        String[] args = new String[]{tempFolder.getAbsolutePath()};
+        JUnRarTestUtil.errorsOcurred = false;
+        ExtractArchive.main(new String[]{testRar.getAbsolutePath(), tempFolder.getAbsolutePath() });
+		JUnRarTestUtil.main(args);
+		File fooDir = new File(tempFolder,"foo");
+		assertTrue(fooDir.exists());
+		File barFile = new File(fooDir,"bar.txt");
+		assertTrue(fooDir.exists());
+		String barTxtContents = FileUtils.readFileToString(barFile);
+		assertEquals("baz\n", barTxtContents);
+		if(errorsOcurred){
+			fail("Test failed, see output for details...");
+		}
+	}
+	
+	@AfterClass
+	public static void tearDownFunctionalTests() throws IOException{
+		FileUtils.deleteDirectory(tempFolder);
 	}
 
 	private static void printSummary()
@@ -98,7 +144,8 @@ public class JUnRarTestUtil
 	private static void testFile(File file)
 	{
 		if(file==null || !file.exists()){
-			logger.error("error file " +file + " does not exist");
+			String errorMessage = "error file " +file + " does not exist";
+			error(errorMessage);
 			return;
 		}
 		logger.info(">>>>>> testing archive: "+file);
@@ -107,13 +154,13 @@ public class JUnRarTestUtil
 		if(s.equalsIgnoreCase("rar")){
 			System.out.println(file.toString());
 			ReadOnlyAccessFile readFile = null;
+			Archive arc = null;
 			try {
 //				readFile = new ReadOnlyAccessFile(file);
-				Archive arc = null;
 				try {
 					arc = new Archive(file);
 				} catch (RarException e) {
-					logger.error("archive consturctor error",e);
+					error("archive consturctor error",e);
 					errorFiles.add(file.toString());
 					return;
 				}
@@ -144,25 +191,25 @@ public class JUnRarTestUtil
 							arc.extractFile(fh, os);
 						} catch (RarException e) {
 							if(e.getType().equals(RarExceptionType.notImplementedYet)){
-								logger.error("error extracting unsupported file: "+fh.getFileNameString(),e);
+								error("error extracting unsupported file: "+fh.getFileNameString(),e);
 								unsupportedFiles.add(file.toString());
 								return;
 							}
-							logger.error("error extracting file: "+fh.getFileNameString(),e);
+							error("error extracting file: "+fh.getFileNameString(),e);
 							errorFiles.add(file.toString());
 							return;
 						}finally{
 							os.close();
-							arc.close();
 						}
 						
 						logger.info("end: "+new Date());
 					}
 				}
+				arc.close();
 				logger.info("successfully tested archive: "+file);
 				successfulFiles.add(file.toString());
 			} catch (Exception e) {
-				logger.error("file: "+file+ " extraction error - does the file exist?"+e );
+				error("file: "+file+ " extraction error - does the file exist?"+e );
 				errorFiles.add(file.toString());
 			} finally{
 				if(readFile!=null){
@@ -175,6 +222,16 @@ public class JUnRarTestUtil
 			}
 			
 		}
+	}
+
+	private static void error(String errorMessage, Exception e) {
+		JUnRarTestUtil.errorsOcurred = true;
+		logger.error(errorMessage,e);
+	}
+
+	private static void error(String errorMessage) {
+		JUnRarTestUtil.errorsOcurred = true;
+		logger.error(errorMessage);
 	}
 	
 	private static void recurseDirectory(File file)
