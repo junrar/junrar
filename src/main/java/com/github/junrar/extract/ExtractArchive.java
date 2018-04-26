033cc89f813835071615829423f0367146720635
Lucas <lucass@gmail.com>
2018-04-26 18:52:53 -0300

    unused imports

--- src/main/java/com/github/junrar/extract/ExtractArchive.java
+++ src/main/java/com/github/junrar/extract/ExtractArchive.java
@@ -4,8 +4,6 @@ import java.io.File;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.OutputStream;
-import java.util.Collections;
-import java.util.List;
 
 import org.apache.commons.logging.Log;
 
