88e8277025a381d1c985ec46ea13d98e20f40e0d
lucass <lucass@gmail.com>
2018-06-17 21:27:18 -0300

    javadoc

--- src/main/java/com/github/junrar/testutil/ExtractArchive.java
+++ src/main/java/com/github/junrar/testutil/ExtractArchive.java
@@ -21,6 +21,15 @@ public class ExtractArchive {
 		}
 	}
 	
+	/**
+	 * @deprecated  As of release 1.0.2, replaced by { @link #Junrar.extract(final String rarPath, final String destinationPath) }
+	 * 
+	 * @param archive rar file path
+	 * @param destination folder where the files will be extracted
+	 * 
+	 * @throws IOException .
+	 * @throws RarException .
+	 */
 	@Deprecated
 	public static void extractArchive(String archive, String destination) throws IOException, RarException {
 		Junrar.extract(archive, destination);
