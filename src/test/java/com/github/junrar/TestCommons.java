7309b1d9c077d5cd59f140c789f32fda8feb0848
theobisproject <theobisproject@gmail.com>
2018-12-28 15:47:32 +0100

    Enable checkstyle rules for test sources

--- src/test/java/com/github/junrar/TestCommons.java
+++ src/test/java/com/github/junrar/TestCommons.java
@@ -1,34 +1,33 @@
 package com.github.junrar;
 
+import com.github.junrar.testUtil.JUnRarTestUtil;
+import org.apache.commons.io.FileUtils;
+import org.apache.commons.io.IOUtils;
+
 import java.io.File;
 import java.io.IOException;
 import java.io.InputStream;
 
-import org.apache.commons.io.FileUtils;
-import org.apache.commons.io.IOUtils;
-
-import com.github.junrar.testUtil.JUnRarTestUtil;
-
 public class TestCommons {
 
-	public static String SIMPLE_RAR_RESOURCE_PATH = "test.rar";
-	
-	public static File createTempDir() throws IOException {
-		final File tmp = File.createTempFile("Junrar", "test");
-		tmp.delete();
-		tmp.mkdir();
-		return tmp;
-	}
+    public static String SIMPLE_RAR_RESOURCE_PATH = "test.rar";
 
-	public static File writeTestRarToFolder(File tmp) throws IOException {
-		return writeResourceToFolder(tmp, SIMPLE_RAR_RESOURCE_PATH);
-	}
+    public static File createTempDir() throws IOException {
+        final File tmp = File.createTempFile("Junrar", "test");
+        tmp.delete();
+        tmp.mkdir();
+        return tmp;
+    }
 
-	public static File writeResourceToFolder(File destination, String resourcePath) throws IOException {
-		InputStream resourceAsStream = JUnRarTestUtil.class.getResourceAsStream(resourcePath);
-		File testRar = new File(destination, resourcePath);
-	    FileUtils.writeByteArrayToFile(testRar, IOUtils.toByteArray(resourceAsStream));
-		return testRar;
-	}
+    public static File writeTestRarToFolder(File tmp) throws IOException {
+        return writeResourceToFolder(tmp, SIMPLE_RAR_RESOURCE_PATH);
+    }
+
+    public static File writeResourceToFolder(File destination, String resourcePath) throws IOException {
+        InputStream resourceAsStream = JUnRarTestUtil.class.getResourceAsStream(resourcePath);
+        File testRar = new File(destination, resourcePath);
+        FileUtils.writeByteArrayToFile(testRar, IOUtils.toByteArray(resourceAsStream));
+        return testRar;
+    }
 
 }
