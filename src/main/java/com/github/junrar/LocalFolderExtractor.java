b2abb7078fe2398067424275553d5b28cf571c96
lucass <lucass@gmail.com>
2019-01-27 00:52:59 +0100

    Add one more test and conform to checkstyle rules

--- src/main/java/com/github/junrar/LocalFolderExtractor.java
+++ src/main/java/com/github/junrar/LocalFolderExtractor.java
@@ -1,7 +1,6 @@
 package com.github.junrar;
 
 import java.io.File;
-import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.OutputStream;
@@ -13,107 +12,110 @@ import org.apache.commons.logging.LogFactory;
 import com.github.junrar.exception.RarException;
 import com.github.junrar.rarfile.FileHeader;
 
-public class LocalFolderExtractor implements ExtractDestination{
+public class LocalFolderExtractor implements ExtractDestination {
 
-	private File folderDestination;
-	private FileSystem fileSystem;
-	private static final Log logger = LogFactory.getLog(LocalFolderExtractor.class);
+    private File folderDestination;
+    private FileSystem fileSystem;
+    private static final Log logger = LogFactory.getLog(LocalFolderExtractor.class);
 
-	public LocalFolderExtractor(final File destination, final FileSystem fileSystem) {
-		this.folderDestination = destination;
-		this.fileSystem = fileSystem;
-	}
-	
-	@Override
-	public File createDirectory(final FileHeader fh) {
-		String fileName = null;
-		if (fh.isDirectory()) {
-			if (fh.isUnicode()) {
-				fileName = fh.getFileNameW();
-			} else {
-				fileName = fh.getFileNameString();
-			}
-		}
+    public LocalFolderExtractor(final File destination, final FileSystem fileSystem) {
+        this.folderDestination = destination;
+        this.fileSystem = fileSystem;
+    }
 
-		if (fileName == null) {
-			return null;
-		}
+    @Override
+    public File createDirectory(final FileHeader fh) {
+        String fileName = null;
+        if (fh.isDirectory()) {
+            if (fh.isUnicode()) {
+                fileName = fh.getFileNameW();
+            } else {
+                fileName = fh.getFileNameString();
+            }
+        }
 
-		File f = new File(folderDestination, fileName);
-		try {
-			if (!f.getCanonicalPath().startsWith(folderDestination.getCanonicalPath())) {
-				throw new IllegalStateException("Detected path traversal! Stop extracting.");
-			}
-		} catch (IOException e) {
-			throw new IllegalStateException(e);
-		}
+        if (fileName == null) {
+            return null;
+        }
 
-		return f;
-	}
-	
-	@Override
-	public File extract(
-		final Archive arch, 
-		final FileHeader fileHeader
-	) throws RarException, IOException {
-		final File f = createFile(fileHeader, folderDestination);
-		OutputStream stream = null;
-		try {
-			stream = new FileOutputStream(f);
-			arch.extractFile(fileHeader, stream);
-		} finally {
-			if (stream != null) {
-				stream.close();
-			}
-		}
-		return f;
-	}
-	
-	private File createFile(final FileHeader fh, final File destination) throws IOException {
-		File f;
-		String name;
-		if (fh.isFileHeader() && fh.isUnicode()) {
-			name = fh.getFileNameW();
-		} else {
-			name = fh.getFileNameString();
-		}
-		f = new File(destination, name);
-		if (!f.getCanonicalPath().startsWith(destination.getCanonicalPath())) {
-			throw new IllegalStateException("Detected path traversal! Stop extracting.");
-		}
-		if (!f.exists()) {
-			try {
-				f = makeFile(destination, name);
-			} catch (final IOException e) {
-				logger.error("error creating the new file: " + f.getName(), e);
-			}
-		}
-		return f;
-	}
-	
-	private File makeFile(final File destination, final String name) throws IOException {
-		final String[] dirs = name.split("\\\\");
-		if (dirs == null) {
-			return null;
-		}
-		String path = "";
-		final int size = dirs.length;
-		if (size == 1) {
-			return new File(destination, name);
-		} else if (size > 1) {
-			for (int i = 0; i < dirs.length - 1; i++) {
-				path = path + File.separator + dirs[i];
-				File dir = new File(destination, path);
-				fileSystem.mkdir(dir);
-			}
-			path = path + File.separator + dirs[dirs.length - 1];
-			final File f = new File(destination, path);
-			fileSystem.createNewFile(f);
-			return f;
-		} else {
-			return null;
-		}
-	}
-	
+        File f = new File(folderDestination, fileName);
+        try {
+            String fileCanonPath = f.getCanonicalPath();
+            if (!fileCanonPath.startsWith(folderDestination.getCanonicalPath())) {
+                String errorMessage = "Rar contains invalid path: '" + fileCanonPath + "'";
+                throw new IllegalStateException(errorMessage);
+            }
+        } catch (IOException e) {
+            throw new IllegalStateException(e);
+        }
+
+        return f;
+    }
+
+    @Override
+    public File extract(
+            final Archive arch,
+            final FileHeader fileHeader
+    ) throws RarException, IOException {
+        final File f = createFile(fileHeader, folderDestination);
+        OutputStream stream = null;
+        try {
+            stream = new FileOutputStream(f);
+            arch.extractFile(fileHeader, stream);
+        } finally {
+            if (stream != null) {
+                stream.close();
+            }
+        }
+        return f;
+    }
+
+    private File createFile(final FileHeader fh, final File destination) throws IOException {
+        String name;
+        if (fh.isFileHeader() && fh.isUnicode()) {
+            name = fh.getFileNameW();
+        } else {
+            name = fh.getFileNameString();
+        }
+        File f = new File(destination, name);
+        String dirCanonPath = f.getCanonicalPath();
+        if (!dirCanonPath.startsWith(destination.getCanonicalPath())) {
+            String errorMessage = "Rar contains file with invalid path: '" + dirCanonPath + "'";
+            throw new IllegalStateException(errorMessage);
+        }
+        if (!f.exists()) {
+            try {
+                f = makeFile(destination, name);
+            } catch (final IOException e) {
+                logger.error("error creating the new file: " + f.getName(), e);
+            }
+        }
+        return f;
+    }
+
+    private File makeFile(final File destination, final String name) throws IOException {
+        final String[] dirs = name.split("\\\\");
+        if (dirs == null) {
+            return null;
+        }
+        String path = "";
+        final int size = dirs.length;
+        if (size == 1) {
+            return new File(destination, name);
+        } else if (size > 1) {
+            for (int i = 0; i < dirs.length - 1; i++) {
+                path = path + File.separator + dirs[i];
+                File dir = new File(destination, path);
+                fileSystem.mkdir(dir);
+            }
+            path = path + File.separator + dirs[dirs.length - 1];
+            final File f = new File(destination, path);
+            fileSystem.createNewFile(f);
+            return f;
+        } else {
+            return null;
+        }
+    }
+
 
 }
--- src/test/java/com/github/junrar/LocalFolderExtractorTest.java
+++ src/test/java/com/github/junrar/LocalFolderExtractorTest.java
@@ -14,25 +14,45 @@ import static org.mockito.Mockito.when;
 
 public class LocalFolderExtractorTest {
 
-	@Test
-	public void rarWithDirectoriesOutsideTarget_ShouldThrowException() throws IOException, RarException {
-		File file = new File("/foo");
-		FileSystem mockFileSystem = mock(FileSystem.class);
-		LocalFolderExtractor localFolderExtractor = new LocalFolderExtractor(file, mockFileSystem);
+    @Test
+    public void rarWithDirectoriesOutsideTarget_ShouldThrowException() throws IOException, RarException {
+        File file = new File("/foo");
+        FileSystem mockFileSystem = mock(FileSystem.class);
+        LocalFolderExtractor localFolderExtractor = new LocalFolderExtractor(file, mockFileSystem);
 
-		Archive archive = mock(Archive.class);
-		FileHeader fileHeader = mock(FileHeader.class);
-		when(fileHeader.isFileHeader()).thenReturn(true);
-		when(fileHeader.isUnicode()).thenReturn(true);
-		when(fileHeader.getFileNameW()).thenReturn("../../passwd");
+        Archive archive = mock(Archive.class);
+        FileHeader fileHeader = mock(FileHeader.class);
+        when(fileHeader.isFileHeader()).thenReturn(true);
+        when(fileHeader.isUnicode()).thenReturn(true);
+        when(fileHeader.getFileNameW()).thenReturn("../../ops");
 
-		try {
-			localFolderExtractor.extract(archive, fileHeader);
-		}catch(IllegalStateException e){
-			Assert.assertEquals("Detected path traversal! Stop extracting.", e.getMessage());
-			return;
-		}
-		Assert.fail("Should have thrown 'java.lang.IllegalStateException: Detected path traversal! Stop extracting.'");
-	}
+        try {
+            localFolderExtractor.extract(archive, fileHeader);
+        } catch (IllegalStateException e) {
+            Assert.assertEquals("Rar contains file with invalid path: '/../ops'", e.getMessage());
+            return;
+        }
+        Assert.fail("Should have thrown 'java.lang.IllegalStateException: Detected path traversal! Stop extracting.'");
+    }
+
+    @Test
+    public void rarWithFileOutsideTarget_ShouldThrowException() throws IOException, RarException {
+        File file = new File("/foo");
+        FileSystem mockFileSystem = mock(FileSystem.class);
+        LocalFolderExtractor localFolderExtractor = new LocalFolderExtractor(file, mockFileSystem);
+
+        FileHeader fileHeader = mock(FileHeader.class);
+        when(fileHeader.isDirectory()).thenReturn(true);
+        when(fileHeader.isUnicode()).thenReturn(true);
+        when(fileHeader.getFileNameW()).thenReturn("../../ops/");
+
+        try {
+            localFolderExtractor.createDirectory(fileHeader);
+        } catch (IllegalStateException e) {
+            Assert.assertEquals("Rar contains invalid path: '/../ops'", e.getMessage());
+            return;
+        }
+        Assert.fail("Should have thrown 'java.lang.IllegalStateException: Detected path traversal! Stop extracting.'");
+    }
 
 }
