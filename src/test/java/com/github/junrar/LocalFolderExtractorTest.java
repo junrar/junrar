afe2d16e44362a51a9adc0b75c51c99109d52037
Gauthier Roebroeck <gauthier.roebroeck@gmail.com>
2020-07-16 21:57:46 +0800

    test: fix failing tests

--- src/test/java/com/github/junrar/LocalFolderExtractorTest.java
+++ src/test/java/com/github/junrar/LocalFolderExtractorTest.java
@@ -1,21 +1,21 @@
 package com.github.junrar;
 
-import com.github.junrar.exception.RarException;
 import com.github.junrar.rarfile.FileHeader;
 import com.github.junrar.vfs2.provider.rar.FileSystem;
-import org.junit.Assert;
 import org.junit.Test;
 
 import java.io.File;
 import java.io.IOException;
 
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.catchThrowable;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 
 public class LocalFolderExtractorTest {
 
     @Test
-    public void rarWithDirectoriesOutsideTarget_ShouldThrowException() throws IOException, RarException {
+    public void rarWithDirectoriesOutsideTarget_ShouldThrowException() throws IOException {
         File file = TestCommons.createTempDir();
         FileSystem mockFileSystem = mock(FileSystem.class);
         LocalFolderExtractor localFolderExtractor = new LocalFolderExtractor(file, mockFileSystem);
@@ -26,19 +26,19 @@ public class LocalFolderExtractorTest {
         when(fileHeader.isUnicode()).thenReturn(true);
         when(fileHeader.getFileNameW()).thenReturn("../../ops");
 
-        try {
-            localFolderExtractor.extract(archive, fileHeader);
-        } catch (IllegalStateException e) {
-            File expectedInvalidPath = new File(file.getParentFile().getParentFile(), "ops");
-            Assert.assertTrue(e.getMessage().contains("Rar contains file with invalid path"));
-            Assert.assertTrue(e.getMessage().contains(expectedInvalidPath.toString()));
-            return;
-        }
-        Assert.fail("Should have thrown 'java.lang.IllegalStateException: Detected path traversal! Stop extracting.'");
+
+        File expectedInvalidPath = new File(file.getParentFile().getParentFile(), "ops");
+
+        Throwable thrown = catchThrowable(() -> localFolderExtractor.extract(archive, fileHeader));
+
+        assertThat(thrown).isInstanceOf(IllegalStateException.class);
+        assertThat(thrown.getMessage())
+            .containsIgnoringCase("Rar contains file with invalid path")
+            .containsIgnoringCase(expectedInvalidPath.toString());
     }
 
     @Test
-    public void rarWithFileOutsideTarget_ShouldThrowException() throws IOException, RarException {
+    public void rarWithFileOutsideTarget_ShouldThrowException() throws IOException {
         File file = TestCommons.createTempDir();
         FileSystem mockFileSystem = mock(FileSystem.class);
         LocalFolderExtractor localFolderExtractor = new LocalFolderExtractor(file, mockFileSystem);
@@ -48,15 +48,14 @@ public class LocalFolderExtractorTest {
         when(fileHeader.isUnicode()).thenReturn(true);
         when(fileHeader.getFileNameW()).thenReturn("../../ops/");
 
-        try {
-            localFolderExtractor.createDirectory(fileHeader);
-        } catch (IllegalStateException e) {
-            File expectedInvalidPath = new File(file.getParentFile().getParentFile(), "ops");
-            Assert.assertTrue(e.getMessage().contains("Rar contains invalid path"));
-            Assert.assertTrue(e.getMessage().contains(expectedInvalidPath.toString()));
-            return;
-        }
-        Assert.fail("Should have thrown 'java.lang.IllegalStateException: Detected path traversal! Stop extracting.'");
+
+        File expectedInvalidPath = new File(file.getParentFile().getParentFile(), "ops");
+        Throwable thrown = catchThrowable(() -> localFolderExtractor.createDirectory(fileHeader));
+
+        assertThat(thrown).isInstanceOf(IllegalStateException.class);
+        assertThat(thrown.getMessage())
+            .containsIgnoringCase("Rar contains invalid path")
+            .containsIgnoringCase(expectedInvalidPath.toString());
     }
 
 }
