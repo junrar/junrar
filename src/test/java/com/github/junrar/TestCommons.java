4a57f8abedca7d0db5a265c8bb061f705c1ce87d
sunny-shu <361442342@qq.com>
2020-07-17 11:55:53 +0800

    fix CI failed

--- src/test/java/com/github/junrar/TestCommons.java
+++ src/test/java/com/github/junrar/TestCommons.java
@@ -8,39 +8,38 @@ import java.io.File;
 import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStream;
-import java.nio.file.Path;
 import java.nio.file.Paths;
 
 public class TestCommons {
 
-	public static String SIMPLE_RAR_RESOURCE_PATH = "test.rar";
+    public static String SIMPLE_RAR_RESOURCE_PATH = "test.rar";
 
-	public static File createTempDir() throws IOException {
-		final File tmp = File.createTempFile("Junrar", "test");
-		tmp.delete();
-		tmp.mkdir();
-		return tmp;
-	}
+    public static File createTempDir() throws IOException {
+        final File tmp = File.createTempFile("Junrar", "test");
+        tmp.delete();
+        tmp.mkdir();
+        return tmp;
+    }
 
-	public static File writeTestRarToFolder(File tmp) throws IOException {
-		return writeResourceToFolder(tmp, SIMPLE_RAR_RESOURCE_PATH);
-	}
+    public static File writeTestRarToFolder(File tmp) throws IOException {
+        return writeResourceToFolder(tmp, SIMPLE_RAR_RESOURCE_PATH);
+    }
 
-	public static File writeResourceToFolder(File destination, String resourcePath) throws IOException {
-		InputStream resourceAsStream = JUnRarTestUtil.class.getResourceAsStream(resourcePath);
-		File testRar = new File(destination, resourcePath);
-		FileUtils.writeByteArrayToFile(testRar, IOUtils.toByteArray(resourceAsStream));
-		return testRar;
-	}
+    public static File writeResourceToFolder(File destination, String resourcePath) throws IOException {
+        InputStream resourceAsStream = JUnRarTestUtil.class.getResourceAsStream(resourcePath);
+        File testRar = new File(destination, resourcePath);
+        FileUtils.writeByteArrayToFile(testRar, IOUtils.toByteArray(resourceAsStream));
+        return testRar;
+    }
 
-	public static void copyRarsToFolder(File destination, File srcDir) throws IOException {
-		String[] fileNames = srcDir.list();
-		for (String fileName : fileNames) {
-			File srcFile = Paths.get(srcDir.getPath(), fileName).toFile();
-			InputStream resourceAsStream = new FileInputStream(srcFile);
-			File destFile = new File(destination, srcFile.getName());
-			FileUtils.writeByteArrayToFile(destFile, IOUtils.toByteArray(resourceAsStream));
-		}
-	}
+    public static void copyRarsToFolder(File destination, File srcDir) throws IOException {
+        String[] fileNames = srcDir.list();
+        for (String fileName : fileNames) {
+            File srcFile = Paths.get(srcDir.getPath(), fileName).toFile();
+            InputStream resourceAsStream = new FileInputStream(srcFile);
+            File destFile = new File(destination, srcFile.getName());
+            FileUtils.writeByteArrayToFile(destFile, IOUtils.toByteArray(resourceAsStream));
+        }
+    }
 
 }
--- src/test/java/com/github/junrar/VolumeExtractorTest.java
+++ src/test/java/com/github/junrar/VolumeExtractorTest.java
@@ -16,54 +16,55 @@ import com.github.junrar.exception.RarException;
 import com.github.junrar.impl.FileVolumeManager;
 import com.github.junrar.vfs2.provider.rar.FileSystem;
 
-
 public class VolumeExtractorTest {
-	private FileSystem fileSystem = new FileSystem();
-	private static File tempFolder;
-	private static String volumesDirName = "volumes";
-	private static String firstVolume = "test-documents.part1.rar";
+    private FileSystem fileSystem = new FileSystem();
+    private static File tempFolder;
+    private static String volumesDirName = "volumes";
+    private static String firstVolume = "test-documents.part1.rar";
 
-	@BeforeClass
-	public static void setupFunctionalTests() throws IOException {
-		tempFolder = TestCommons.createTempDir();
-		File dir = new File(VolumeExtractorTest.class.getResource("").getPath(), volumesDirName);
-		TestCommons.copyRarsToFolder(tempFolder, dir);
-	}
+    @BeforeClass
+    public static void setupFunctionalTests() throws IOException {
+        tempFolder = TestCommons.createTempDir();
+        File dir = new File(VolumeExtractorTest.class.getResource("").getPath(), volumesDirName);
+        TestCommons.copyRarsToFolder(tempFolder, dir);
+    }
 
-	@AfterClass
-	public static void tearDownFunctionalTests() throws IOException {
-		FileUtils.deleteDirectory(tempFolder);
-	}
+    @AfterClass
+    public static void tearDownFunctionalTests() throws IOException {
+        FileUtils.deleteDirectory(tempFolder);
+    }
 
-	@Test
-	public void extractionFromvolumedFile() throws RarException, IOException {
-		final File rarFileOnTemp = new File(tempFolder, firstVolume);
-		final File unpackedDir = new File(tempFolder, "test-documents");
-		unpackedDir.delete();
-		unpackedDir.mkdir();
-		Junrar.extract(new LocalFolderExtractor(unpackedDir, fileSystem), new FileVolumeManager(rarFileOnTemp));
+    @Test
+    public void extractionFromvolumedFile() throws RarException, IOException {
+        final File rarFileOnTemp = new File(tempFolder, firstVolume);
+        final File unpackedDir = new File(tempFolder, "test-documents");
+        unpackedDir.delete();
+        unpackedDir.mkdir();
+        Junrar.extract(new LocalFolderExtractor(unpackedDir, fileSystem), new FileVolumeManager(rarFileOnTemp));
 
-		checkContent(unpackedDir);
-	}
+        checkContent(unpackedDir);
+    }
 
-	private void checkContent(File unpackedDir) {
-		String[] fileNames = unpackedDir.list();
-		List<ContentDescription> contentDescriptions = new ArrayList<ContentDescription>();
-		for (String fileName : fileNames) {
-			File file = new File(unpackedDir, fileName);
-			contentDescriptions.add(new ContentDescription(file.getName(), file.length()));
-		}
+    private void checkContent(File unpackedDir) {
+        String[] fileNames = unpackedDir.list();
+        List<ContentDescription> contentDescriptions = new ArrayList<ContentDescription>();
+        for (String fileName : fileNames) {
+            File file = new File(unpackedDir, fileName);
+            contentDescriptions.add(new ContentDescription(file.getName(), file.length()));
+        }
 
-		final ContentDescription[] expected = { new ContentDescription("testEXCEL.xls", 13824),
-				new ContentDescription("testHTML.html", 167), 
-				new ContentDescription("testOpenOffice2.odt", 26448),
-				new ContentDescription("testPDF.pdf", 34824), 
-				new ContentDescription("testPPT.ppt", 16384),
-				new ContentDescription("testRTF.rtf", 3410), 
-				new ContentDescription("testTXT.txt", 49),
-				new ContentDescription("testWORD.doc", 19456), 
-				new ContentDescription("testXML.xml", 766) };
+        final ContentDescription[] expected = {
+                new ContentDescription("testEXCEL.xls", 13824),
+                new ContentDescription("testHTML.html", 167),
+                new ContentDescription("testOpenOffice2.odt", 26448),
+                new ContentDescription("testPDF.pdf", 34824),
+                new ContentDescription("testPPT.ppt", 16384),
+                new ContentDescription("testRTF.rtf", 3410),
+                new ContentDescription("testTXT.txt", 49),
+                new ContentDescription("testWORD.doc", 19456),
+                new ContentDescription("testXML.xml", 766)
+        };
 
-		assertArrayEquals(expected, contentDescriptions.toArray());
-	}
+        assertArrayEquals(expected, contentDescriptions.toArray());
+    }
 }
