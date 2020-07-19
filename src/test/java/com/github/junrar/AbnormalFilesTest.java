dfbd04783228e41aa603b33275acccea4c03b1a3
Gauthier Roebroeck <gauthier.roebroeck@gmail.com>
2020-07-19 20:48:28 +0800

    refactor: use JUnit Arguments

--- src/test/java/com/github/junrar/AbnormalFilesTest.java
+++ src/test/java/com/github/junrar/AbnormalFilesTest.java
@@ -7,13 +7,12 @@ import org.apache.commons.io.FileUtils;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
 
 import java.io.File;
 import java.io.IOException;
 import java.io.InputStream;
-import java.util.HashMap;
-import java.util.Map;
 import java.util.stream.Stream;
 
 import static org.assertj.core.api.Assertions.assertThat;
@@ -36,32 +35,30 @@ public class AbnormalFilesTest {
 
     @ParameterizedTest
     @MethodSource("provideFilesAndExpectedExceptionType")
-    public void extractFile(Map.Entry<String, Class<?>> fileAndResult) throws Exception {
-        File file = new File(getClass().getResource(fileAndResult.getKey()).toURI());
+    public void extractFile(String filePath, Class<?> expectedException) throws Exception {
+        File file = new File(getClass().getResource(filePath).toURI());
 
         Throwable thrown = catchThrowable(() -> Junrar.extract(file, tempDir));
 
         assertThat(thrown).isInstanceOf(RarException.class);
-        assertThat(thrown).isExactlyInstanceOf(fileAndResult.getValue());
+        assertThat(thrown).isExactlyInstanceOf(expectedException);
     }
 
     @ParameterizedTest
     @MethodSource("provideFilesAndExpectedExceptionType")
-    public void extractFromStream(Map.Entry<String, Class<?>> fileAndResult) throws Exception {
-        try (InputStream stream = getClass().getResourceAsStream(fileAndResult.getKey())) {
+    public void extractFromStream(String filePath, Class<?> expectedException) throws Exception {
+        try (InputStream stream = getClass().getResourceAsStream(filePath)) {
             Throwable thrown = catchThrowable(() -> Junrar.extract(stream, tempDir));
 
             assertThat(thrown).isInstanceOf(RarException.class);
-            assertThat(thrown).isExactlyInstanceOf(fileAndResult.getValue());
+            assertThat(thrown).isExactlyInstanceOf(expectedException);
         }
     }
 
-    private static Stream<Map.Entry<String, Class<?>>> provideFilesAndExpectedExceptionType() {
-        Map<String, Class<?>> map = new HashMap<>();
-
-        map.put("abnormal/corrupt-header.rar", CorruptHeaderException.class);
-        map.put("abnormal/mainHeaderNull.rar", MainHeaderNullException.class);
-
-        return map.entrySet().stream();
+    private static Stream<Arguments> provideFilesAndExpectedExceptionType() {
+        return Stream.of(
+            Arguments.of("abnormal/corrupt-header.rar", CorruptHeaderException.class),
+            Arguments.of("abnormal/mainHeaderNull.rar", MainHeaderNullException.class)
+        );
     }
 }
