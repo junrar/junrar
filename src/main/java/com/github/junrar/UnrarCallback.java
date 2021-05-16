e29de056c47f7e2d869d507e01fa882d04f420ac
Mingun <Alexander_Sergey@mail.ru>
2021-05-16 20:19:13 +0500

    Replace deprecated <tt>XXX</tt> tag with {@code XXX}

--- src/main/java/com/github/junrar/UnrarCallback.java
+++ src/main/java/com/github/junrar/UnrarCallback.java
@@ -11,10 +11,8 @@ public interface UnrarCallback {
     /**
      * @param nextVolume ,
      *
-     * @return <tt>true</tt> if the next volume is ready to be processed,
-     * <tt>false</tt> otherwise.
-     *
-     *
+     * @return {@code true} if the next volume is ready to be processed,
+     *         {@code false} otherwise.
      */
     boolean isNextVolumeReady(Volume nextVolume);
 
